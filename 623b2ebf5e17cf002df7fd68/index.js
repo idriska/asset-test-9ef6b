fc2c5a7734e34a3c0703a8fa7645e730a621807a
idriska <idriscumali@gmail.com>
2022-03-28 14:22:48 +0300

    auth + mailler

--- 623b2ebf5e17cf002df7fd68/index.js
+++ 623b2ebf5e17cf002df7fd68/index.js
@@ -7,7 +7,7 @@ const nodeMailerPassword = process.env.SMTP_PASSWORD || null;
 const mailFrom = process.env.MAIL_FROM || null
 
 export default async function(change) {
-
+    console.log("TEST 2")
     console.log("Sending a mail. Parameters are: ", change);
 
     let buckets = {
